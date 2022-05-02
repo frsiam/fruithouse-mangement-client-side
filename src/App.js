cf888f3481eb33738458a41bb908e64f0adb52fc
frsiam <siam.frs@gmail.com>
2022-05-02 11:57:48 +0600

    header design complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
+        <nav className="navbar sticky-top navbar-expand-md navbar-dark bg-dark">
+            <div className="container-fluid py-2 mx-md-3">
+                <Link className="navbar-brand" to="/"><span className='fs-4 fw-bolder text-uppercase'><span className='border-y-2 text-green-400'>Fruits</span> <span className='text-orange-400 border-y-2 border-info'>house</span></span></Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="blogs">Blogs</Link>
+                        </li>
+                    </ul>
+                    {/* <div className="d-flex align-items-center">
+                        {user && <p className='mt-3 fw-bold'>{user.displayName}</p>}
+                        {
+                            user ?
+                                <Link to='login' onClick={handleSignOut} className="btn btn-link text-decoration-none" type="submit">Sign Out</Link>
+                                : <Link to='login' className='nav-link'>Login</Link>}
+                    </div> */}
+                </div>
+            </div>
+        </nav>
     );
 };
 
