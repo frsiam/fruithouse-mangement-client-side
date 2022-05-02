0b3928b9afc79b5a247de9b78885e81562377a01
frsiam <siam.frs@gmail.com>
2022-05-02 13:00:57 +0600

    hero area added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BeakerIcon, HomeIcon, MailIcon, PhoneIcon, PhoneIncomingIcon } from '@heroicons/react/solid'
 
 const Footer = () => {
     const year = new Date().getFullYear();
@@ -68,7 +69,7 @@ const Footer = () => {
                         <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                             {/* <!-- Content --> */}
                             <h6 className="text-uppercase fw-bold mb-4">
-                                <i className="fas fa-gem me-3"></i>Company name
+                                About Developer
                             </h6>
                             <p>
                                 Here you can use rows and columns to organize your footer content. Lorem ipsum
@@ -125,13 +126,13 @@ const Footer = () => {
                             <h6 className="text-uppercase fw-bold mb-4">
                                 Contact
                             </h6>
-                            <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                            <p><HomeIcon className="h-5 w-5 text-blue-500 d-inline" /> New York, NY 10012, US</p>
                             <p>
-                                <i className="fas fa-envelope me-3"></i>
+                                <MailIcon className="h-5 w-5 text-blue-500 d-inline" />
                                 info@example.com
                             </p>
-                            <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                            <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+                            <p><PhoneIcon className="h-5 w-5 text-blue-500 d-inline" /> + 88 019 30 820 152</p>
+                            <p><i className="fas fa-print me-3"></i> + 015 80 878 858</p>
                         </div>
                         {/* <!-- Grid column --> */}
                     </div>
@@ -142,7 +143,7 @@ const Footer = () => {
 
             {/* <!-- Copyright --> */}
             <div className="text-center p-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-                © 2021 Copyright:
+                © {year} Copyright:
                 <a className="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
             </div>
             {/* <!-- Copyright --> */}
