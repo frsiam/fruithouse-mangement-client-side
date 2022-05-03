aaba9edb0afd7f731fb2fcc6f0414501bfa3be5d
frsiam <siam.frs@gmail.com>
2022-05-04 00:04:01 +0600

    design the login and register form and toggle

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import { EyeIcon } from '@heroicons/react/solid';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const navigate = useNavigate();
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email);
+        console.log(password);
+    }
     return (
-        <div className='bg-black min-h-screen'>
-            <div className='container mx-auto w-3/4 md:w-1/2 lg:w-2/6 p-5 bg-emerald-200'>
-                <h1 className='text-2xl text-center my-3 font-semibold'>Login form</h1>
-                <form className='my-5'>
+        <div className=' bg-black min-h-screen'>
+            <div className='container  mx-auto w-3/4 md:w-1/2 lg:w-2/6 p-5 bg-emerald-200'>
+                <h1 className='text-2xl text-center mt-5 mb-3 font-semibold'>Login form</h1>
+                <form className='my-5' onSubmit={handleSubmit}>
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <label htmfor="exampleInputEmail1" className="form-label">Email address</label>
+                        <input ref={emailRef} type="email" className="form-control text-xl" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                        <label htmfor="exampleInputPassword1" className="form-label">Password</label>
+                        <input ref={passwordRef} type="password" className="form-control position-relative" id="exampleInputPassword1" />
                     </div>
                     <div className="mb-3 form-check">
                         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                        <label className="form-check-label" htmfor="exampleCheck1">Check me out</label>
                     </div>
                     <button className="btn btn-primary rounded-0 w-full">Submit</button>
                 </form>
+                <p>Not a Member ? <span onClick={() => navigate('/register')} className='text-primary cursor-pointer'>Register</span></p>
             </div>
         </div>
     );
--- src/pages/Login/Register.js
+++ src/pages/Login/Register.js
@@ -1,30 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const navigate = useNavigate();
     return (
         <div className='bg-black min-h-screen'>
-            <div className='container mx-auto w-3/4 md:w-1/2 lg:w-2/6 p-5 bg-emerald-200'>
-                <h1 className='text-2xl text-center my-3 font-semibold'>Login form</h1>
-                <form className='my-5'>
+            <div className='container mx-auto w-3/4 md:w-1/2 lg:w-2/6 p-4 bg-emerald-200'>
+                <h1 className='text-2xl text-center  mt-5 font-semibold'>Please Register</h1>
+                <form className='my-3'>
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Email address</label>
+                        <label htmfor="exampleInputEmail1" className="form-label">Email address</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <label htmfor="exampleInputPassword1" className="form-label">Password</label>
                         <input type="password" className="form-control" id="exampleInputPassword1" />
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label">Confirm Password</label>
+                        <label htmfor="exampleInputPassword1" className="form-label">Confirm Password</label>
                         <input type="password" className="form-control" id="exampleInputPassword1" />
                     </div>
                     <div className="mb-3 form-check">
                         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                        <label className="form-check-label" htmfor="exampleCheck1">Check me out</label>
                     </div>
                     <button className="btn btn-primary rounded-0 w-full">Submit</button>
                 </form>
+                <p>Already Member ? <span onClick={() => navigate('/login')} className='text-primary cursor-pointer'>Please Login</span></p>
             </div>
         </div>
     );
